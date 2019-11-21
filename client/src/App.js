330ede26ef800781d921d647d509927a7636fa44
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-11-21 01:56:46 +0000

    Added React App

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+import OrderForm from './components/OrderForm/OrderForm';
+
+function App() {
+  return (
+    <div className="App">
+			<header className="header">
+
+			</header>
+			<main className="main">
+				<OrderForm />
+			</main>
+			<footer className="footer">
+
+			</footer>
+    </div>
+  );
+}
+
+export default App;
