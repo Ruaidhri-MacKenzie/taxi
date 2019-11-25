e0601336b72b0fb063ebfbe8ad93409d03edf259
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-11-25 23:36:57 +0000

    Added leaflet for displaying street map

--- client/src/components/StreetMap/StreetMap.js
+++ client/src/components/StreetMap/StreetMap.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import L from 'leaflet';
 import './StreetMap.scss';
 
 const StreetMap = () => {
+	const id = "map";
+	const [latlng, setLatlng] = useState([57.1497, -2.0943]);
+	
+	const loadMap = () => {
+		const osm = L.map(id, {
+			center: latlng,
+			zoom: 13,
+			layers: [
+				L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+					attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+				}),
+			],
+		});
+
+		L.marker(latlng).addTo(osm);
+	};
+	
+	const moveMarker = () => {
+		
+	};
+
+	useEffect(loadMap, []);
+	useEffect(moveMarker, [latlng]);
+
 	return (
-		<div className="map">
-			
-		</div>
+		<div id={id} className="map" onClick={e => setLatlng(e.latlng)}></div>
 	);
 };
 
