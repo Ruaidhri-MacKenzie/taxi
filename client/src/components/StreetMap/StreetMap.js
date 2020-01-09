ad640d9db57bea4fc2eaaeb781b1c20537587780
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-09 21:45:27 +0000

    Store map as ref

--- client/src/components/StreetMap/StreetMap.js
+++ client/src/components/StreetMap/StreetMap.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet';
 import './StreetMap.scss';
 
@@ -6,9 +6,10 @@ const StreetMap = () => {
 	const id = "map";
 	const [latlng, setLatlng] = useState([57.1497, -2.0943]);
 	
-	const loadMap = () => {
-		const osm = L.map(id, {
-			center: latlng,
+	const mapRef = useRef(null);
+	useEffect(() => {
+		mapRef.current = L.map(id, {
+			center: [57.1497, -2.0943],
 			zoom: 13,
 			layers: [
 				L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -16,19 +17,22 @@ const StreetMap = () => {
 				}),
 			],
 		});
-
-		L.marker(latlng).addTo(osm);
-	};
+	}, []);
 	
-	const moveMarker = () => {
-		
-	};
+	const markerRef = useRef(null);
+	useEffect(() => {
+		markerRef.current = L.marker(latlng).addTo(mapRef.current);
+	}, [latlng]);
 
-	useEffect(loadMap, []);
-	useEffect(moveMarker, [latlng]);
+	const clickMap = e => {
+		console.log(e);
+		const { lat, lng } = mapRef.current.mouseEventToLatLng(e.originalEvent);
+		console.log(lat + ', ' + lng);
+		setLatlng([lat, lng]);
+	};
 
 	return (
-		<div id={id} className="map" onClick={e => setLatlng(e.latlng)}></div>
+		<div id={id} className="map" onClick={clickMap}></div>
 	);
 };
 
