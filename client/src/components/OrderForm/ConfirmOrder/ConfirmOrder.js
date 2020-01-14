d1315cd1f769a1d5b72235aa34b6ee4d9cf34820
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-14 22:05:35 +0000

    Removed FormNav, added Address display components

--- client/src/components/OrderForm/ConfirmOrder/ConfirmOrder.js
+++ client/src/components/OrderForm/ConfirmOrder/ConfirmOrder.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './ConfirmOrder.scss';
 
-import FormNav from '../FormNav/FormNav';
+import Address from '../../Address/Address';
 
-const ConfirmOrder = ({ name, tel, location, destination, time, backPage, submitOrder }) => {
+const ConfirmOrder = ({ name, tel, time, locationAddress, destinationAddress }) => {
 	return (
 		<section className="confirm">
 			<div className="confirm__details">
 				<p className="confirm__label">Name: </p><p className="confirm__detail">{name}</p>
 				<p className="confirm__label">Phone: </p><p className="confirm__detail">{tel}</p>
-				<p className="confirm__label">Location: </p><p className="confirm__detail">{location}</p>
-				<p className="confirm__label">Destination: </p><p className="confirm__detail">{destination}</p>
+				<p className="confirm__label">Location: </p><Address address={locationAddress} />
+				<p className="confirm__label">Destination: </p><Address address={destinationAddress} />
 				<p className="confirm__label">Pick-up Time: </p><p className="confirm__detail">{(time || "ASAP")}</p>
 			</div>
-			<FormNav back={backPage} submit={submitOrder} />
 		</section>
 	);
 };
