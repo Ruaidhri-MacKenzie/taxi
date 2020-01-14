7fb80a6a98157e14ba45ae20c2d07baa190ffafc
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-14 23:28:07 +0000

    Added alignment options to Address component

--- client/src/components/Address/Address.js
+++ client/src/components/Address/Address.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Address.scss';
 
-const Address = ({ address }) => {
+const Address = ({ address, align = "center" }) => {
 	if (!address) return null;
 	
 	const { number, street, city, county, postcode } = address;
-
+	const alignClassName = (align === "left") ? " address--left"
+									: (align === "center") ? " address-center"
+									:	(align === "right") ? " address-right"
+									: "";
 	return (
-		<ol className="address">
+		<ol className={"address" + alignClassName}>
 			<li className="address__line">{number + " " + street}</li>
 			<li className="address__line">{city}</li>
 			<li className="address__line">{county}</li>
