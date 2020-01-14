9b1009f205605b2887fc394f3b90ab52572a2c77
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-14 23:29:21 +0000

    Added alignment options to Address component

--- client/src/components/OrderForm/ConfirmOrder/ConfirmOrder.js
+++ client/src/components/OrderForm/ConfirmOrder/ConfirmOrder.js
@@ -9,8 +9,8 @@ const ConfirmOrder = ({ name, tel, time, locationAddress, destinationAddress }) 
 			<div className="confirm__details">
 				<p className="confirm__label">Name: </p><p className="confirm__detail">{name}</p>
 				<p className="confirm__label">Phone: </p><p className="confirm__detail">{tel}</p>
-				<p className="confirm__label">Location: </p><Address address={locationAddress} />
-				<p className="confirm__label">Destination: </p><Address address={destinationAddress} />
+				<p className="confirm__label">Location: </p><Address address={locationAddress} align="left" />
+				<p className="confirm__label">Destination: </p><Address address={destinationAddress} align="left" />
 				<p className="confirm__label">Pick-up Time: </p><p className="confirm__detail">{(time || "ASAP")}</p>
 			</div>
 		</section>
