f994d62e1b252916f36dbcfa4e0416a61ef7061e
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-14 22:11:10 +0000

    Added OrderNav, added lookUpAdddress function as dummy geocoding data, split state into separate variables

--- client/src/components/OrderForm/OrderForm.js
+++ client/src/components/OrderForm/OrderForm.js
@@ -1,72 +1,109 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './OrderForm.scss';
 
+import Error from '../Error/Error';
+import OrderHeader from './OrderHeader/OrderHeader';
+import OrderNav from './OrderNav/OrderNav';
 import AccountDetails from './AccountDetails/AccountDetails';
 import Location from './Location/Location';
 import Destination from './Destination/Destination';
 import PickUpTime from './PickUpTime/PickUpTime';
 import ConfirmOrder from './ConfirmOrder/ConfirmOrder';
 
+const lookUpAddress = latlng => {
+	// Reverse geocoding
+	return {
+		number: "123",
+		street: "Fake Street",
+		city: "Nottatown",
+		county: "Nowhereshire",
+		postcode: "AB12 3XY",
+	};
+};
+
+// const lookUpLatlng = address => {
+// 	// Geocoding
+// 	return [57.1497, -2.0943];
+// };
+
 const OrderForm = () => {
 	/* State */
-	const [state, setState] = useState({
-		page: 0,
-		name: "",
-		tel: "",
-		time: "",
-		location: "",
-		destination: "",
-	});
+	const [page, setPage] = useState(0);
+	const [error, setError] = useState("");
 
-	const setTime = t => setState({ ...state, time: t });
+	const [name, setName] = useState("");
+	const [tel, setTel] = useState("");
+	const [time, setTime] = useState("");
+	const [location, setLocation] = useState([57.1497, -2.0943]);
+	const [locationAddress, setLocationAddress] = useState(lookUpAddress(location));
+	const [destination, setDestination] = useState([57.1497, -2.0943]);
+	const [destinationAddress, setDestinationAddress] = useState(lookUpAddress(destination));
 	
+	const updateLocation = latlng => {
+		setLocation(latlng);
+		setLocationAddress(lookUpAddress(latlng));
+	};
+	const updateDestination = latlng => {
+		setDestination(latlng);
+		setDestinationAddress(lookUpAddress(latlng));
+	};
+	const backPage = () => setPage(page - 1);
+	const nextPage = () => setPage(page + 1);
+
+	const validateDetails = () => {
+		if (!name) {
+			setError("Name is required.");
+			return false;
+		}
+		else if (name.length < 3) {
+			setError("Name must be at least 3 characters long.");
+			return false;
+		}
+		else if (!tel) {
+			setError("Phone number is required.");
+			return false;
+		}
+		else if (!tel.match(/^\s*\(?(020[7,8]{1}\)?[ ]?[1-9]{1}[0-9{2}[ ]?[0-9]{4})|(0[1-8]{1}[0-9]{3}\)?[ ]?[1-9]{1}[0-9]{2}[ ]?[0-9]{3})\s*$/)) {
+			setError("Phone number must be in UK format.");
+			return false;
+		}
+		else {
+			setError("");
+			return true;
+		}
+	};
+
+	const submitOrder = () => {
+		if (validateDetails()) {
+			axios.post('/order', { name, tel, time, location, locationAddress, destination, destinationAddress });
+			console.log(`Name: ${name}, Tel: ${tel}, Time: ${time}, Location: ${location}, Destination: ${destination}.`);
+		}
+	};
+
 	/* Event Handlers */
-	const handleChange = e => setState({ ...state, [e.currentTarget.name]: e.currentTarget.value});
 	const handleSubmit = e => e.preventDefault();
 	
-	const backPage = () => setState({ ...state, page: state.page - 1 });
-	const nextPage = () => {
-		// TODO: validate inputs
-		setState({ ...state, page: state.page + 1 });
-	};
-	const submitOrder = () => {
-		// TODO: validate inputs, axios http post
-		console.log(`Name: ${state.name}, Tel: ${state.tel}, Time: ${state.time}, Location: ${state.location}, Destination: ${state.destination}.`);
-	};
-
 	/* Render */
-	const renderHeader = () => {
-		let title = "";
-		if (state.page === 0) title = "Enter Your Details";
-		else if (state.page === 1) title = "Select Pick-Up Location";
-		else if (state.page === 2) title = "Select Destination";
-		else if (state.page === 3) title = "Select Pick-Up Time";
-		else if (state.page === 4) title = "Confirm Details";
-
-		return (
-			<header className="order__header">
-				<h2 className="order__title">{title}</h2>
-			</header>
-		);
-	};
-
 	const renderBody = () => {
-		switch (state.page) {
-			case 0: return <AccountDetails name={state.name} tel={state.tel} handleChange={handleChange} nextPage={nextPage} />
-			case 1: return <Location location={state.location} handleChange={handleChange} backPage={backPage} nextPage={nextPage} />
-			case 2: return <Destination destination={state.destination} handleChange={handleChange} backPage={backPage} nextPage={nextPage} />
-			case 3: return <PickUpTime time={state.time} setTime={setTime} handleChange={handleChange} backPage={backPage} nextPage={nextPage} />
-			case 4: return <ConfirmOrder { ...state } backPage={backPage} submitOrder={submitOrder} />
+		switch (page) {
+			case 0: return <AccountDetails name={name} setName={setName} tel={tel} setTel={setTel} />
+			case 1: return <Location location={location} setLocation={updateLocation} />
+			case 2: return <Destination destination={destination} setDestination={updateDestination} />
+			case 3: return <PickUpTime time={time} setTime={setTime} />
+			case 4: return <ConfirmOrder {...{ name, tel, time, locationAddress, destinationAddress }} />
 			default:
-				setState({ ...state, page: 0 });
+				setPage(0);
 				return null;
 		}
 	};
 	
 	return (
 		<form className="order" onSubmit={handleSubmit}>
-			{renderHeader()}
+			<OrderHeader page={page} />
 			{renderBody()}
+			{error && <Error error={error} />}
+			<OrderNav page={page} back={backPage} next={nextPage} submit={submitOrder} validateDetails={validateDetails} />
 		</form>
 	);
 };
