186e773a66ed551bdb5cbe434e4646fc155a279d
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-01-14 22:04:28 +0000

    Moved order form header to OrderHeader component

---
+++ client/src/components/OrderForm/OrderHeader/OrderHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './OrderHeader.scss';
+
+const OrderHeader = ({ page }) => {
+	const getTitle = () => {
+		if (page === 0) return "Enter Your Details";
+		else if (page === 1) return "Select Pick-Up Location";
+		else if (page === 2) return "Select Destination";
+		else if (page === 3) return "Select Pick-Up Time";
+		else if (page === 4) return "Confirm Details";
+	};
+
+	return (
+		<header className="order-header">
+			<h2 className="order-header__title">{getTitle()}</h2>
+		</header>
+	);
+};
+
+export default OrderHeader;
